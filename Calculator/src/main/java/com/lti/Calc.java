b794ddfa52ca2b953f255b4889f9e3a6ab67520c
Jayashree Gopal <Jayashree Gopal@Jays-NoteBook>
2020-12-21 15:16:26 +0530

    Updated!!

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -11,10 +11,14 @@ public class Calc {
 	public static int prod(int n1,int n2){
 		return n1*n2;
 	}
+	public static int div(int n1,int n2){
+		return n1/n2;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Add="+add(5,4));
 		System.out.println("Sub="+sub(5,4));
 		System.out.println("Prod="+prod(5,4));
+		System.out.println("Quotient="+div(5,4));
 	}
 }
