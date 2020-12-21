17e09b34e422ccf7fce4bd40603173b5c5b5baa7
Jayashree Gopal <Jayashree Gopal@Jays-NoteBook>
2020-12-21 14:58:37 +0530

    Updated!!

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,9 +5,12 @@ public class Calc {
 	public static int add(int n1,int n2){
 		return n1+n2;
 	}
+	public static int sub(int n1,int n2){
+		return n1-n2;
+	}
 
 	public static void main(String[] args) {
 		System.out.println(add(5,4));
-
+		System.out.println(sub(5,4));
 	}
 }
